7281594370cda404cc22c1e2b27f09d28ea95dd1
azharul-islam-bappy <info.azharulbappy@gmail.com>
2024-11-12 18:46:27 +0600

    design layout almost completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary":{
+        "primary-color":{
           "left-chat": "hsla(276,55%,52%,1)",
           "right-chat": "hsla(271,15%,43%,1)",
           "gray-violet": "hsla(270,7%,64%,1)",
-          "placeholder": "hsla(206,6%,79%,1)"
+          "placeholder": "hsla(26,7%,79%,0.5)"
         },
         "gradient-color": {
           "magenta": "hsla(293,100%,63%,1)",
           "violet": "hsla(264,100%,61%,1)"
         },
-        "secondary": {
-          "white": "fff",
-          "app-bg": "hsla(270,20%,96%,1)",
+        "secondary-color": {
+          "white": "#fff",
+          "chat-bgrn": "hsla(270,20%,96%,1)",
           "submit-btn": "hsla(271,36%,24%,1)",
           "radio-outline": "hsla(289,100%,72%,1)"
         }
